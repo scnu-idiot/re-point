41501c097be8218c4aff245a202289d8e279636e
Hbin77 <phb007298@gmail.com>
2025-08-13 19:39:53 +0900

    챗봇api적용

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,22 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:camera_web/camera_web.dart';
+import 'package:kakao_flutter_sdk_common/src/web/kakao_flutter_sdk_plugin.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  CameraPlugin.registerWith(registrar);
+  KakaoFlutterSdkPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
