24f2debb597637b7e2b178c750be95dd13abf615
Hbin77 <phb007298@gmail.com>
2025-08-23 21:35:11 +0900

    point, user api구축

---
+++ re-point/src/main/java/com/idiot/re_point/RePointApplication.java
@@ -0,0 +1,13 @@
+package com.idiot.re_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RePointApplication.class, args);
+	}
+
+}
---
+++ re-point/src/test/java/com/idiot/re_point/RePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idiot.re_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
